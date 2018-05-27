5366155c14b7ad2bbea5d25ec5bd631eec21c6d0
Helen Balan <helen.v.balan@gmail.com>
2018-05-27 19:34:53 -0400

    Improve some code - 4

--- src/main/java/com/elenabalan/dbtest/entity_random_file/Result.java
+++ src/main/java/com/elenabalan/dbtest/entity_random_file/Result.java
@@ -1,17 +1,17 @@
 package com.elenabalan.dbtest.entity_random_file;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.util.List;
 
 public class Result {
 
-    private ArrayList<File> files;
+    private List<File> files;
 
-    public ArrayList<File> getFiles() {
+    public List<File> getFiles() {
         return files;
     }
 
-    public void setFiles(ArrayList<File> files) {
+    public void setFiles(List<File> files) {
         this.files = files;
     }
 
